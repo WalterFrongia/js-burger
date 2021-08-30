593cf74601a72ea2bbcadb9db6cbf2b35f4beed8
WalterFrongia <walter.frongia@libero.it>
2021-08-30 17:27:04 +0200

    Impostata base html

---
+++ myscript.js
@@ -0,0 +1,7 @@
+// Il programma dovrà consentire di calcolare il prezzo del panino
+// selezionando o deselezionando gli ingredienti proposti.
+
+// Consigli del giorno:
+// Create inizialmente un html base con gli elementi minimi necessari al funzionamento,
+// passate poi alla parte JS ragionando come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice.
